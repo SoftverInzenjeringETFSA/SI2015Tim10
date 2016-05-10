dde9af66687787771bf9ac6dda96f54f32ae9a2a
mirelapeskovic <mirelapeskovic@gmail.com>
2016-05-10 13:37:23 +0200

    Dodana veza Kategorija sama sa sobom

--- Implementacija/SiDesk/src/main/java/ba/unsa/etf/si/app/SiDesk/Model/Kategorija.java
+++ Implementacija/SiDesk/src/main/java/ba/unsa/etf/si/app/SiDesk/Model/Kategorija.java
@@ -1,8 +1,50 @@
 package ba.unsa.etf.si.app.SiDesk.Model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Kategorija {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Kategorija implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue
+	long id;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	@OneToOne
+	@JoinColumn(name="parent_id")
+	private Kategorija kategorija;
+	
+	public Kategorija getKategorija() {
+		return kategorija;
+	}
+
+	public void setKategorija(Kategorija kategorija) {
+		this.kategorija = kategorija;
+	}
+
+	@OneToOne(mappedBy="kategorija")
+	
+	
+	
 
 	private String ime;
 	private String putanja;
