b0484b2992b9d1f76b2c79685a194c78ee12d0f6
Amela Špica <amela.spica@gmail.com>
2016-05-10 10:50:40 +0200

    hibernate

--- Implementacija/SiDesk/src/main/java/ba/unsa/etf/si/app/SiDesk/App.java
+++ Implementacija/SiDesk/src/main/java/ba/unsa/etf/si/app/SiDesk/App.java
@@ -1,5 +1,12 @@
 package ba.unsa.etf.si.app.SiDesk;
 
+
+import javax.transaction.Transaction;
+
+import org.hibernate.Session;
+
+import ba.unsa.etf.si.app.SiDesk.Util.HibernateUtil;
+
 /**
  * Hello world!
  *
@@ -8,7 +15,9 @@ public class App
 {
     public static void main( String[] args )
     {
+    	Session session = (Session) HibernateUtil.getSessionFactory().openSession();
         System.out.println( "Hello World!" );
+        Transaction t = (Transaction) session.beginTransaction();
         
     }
 }
