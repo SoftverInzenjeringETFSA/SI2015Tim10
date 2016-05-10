0fd8b7ceedfe67c28b67523a808ee20ec9ae86ae
Amela Špica <amela.spica@gmail.com>
2016-05-10 10:51:51 +0200

    hibernate

--- Implementacija/SiDesk/src/main/java/ba/unsa/etf/si/app/SiDesk/Util/HibernateUtil.java
+++ Implementacija/SiDesk/src/main/java/ba/unsa/etf/si/app/SiDesk/Util/HibernateUtil.java
@@ -3,6 +3,7 @@ package ba.unsa.etf.si.app.SiDesk.Util;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+
 public class HibernateUtil {
 
 	  private static final SessionFactory sessionFactory = buildSessionFactory();
