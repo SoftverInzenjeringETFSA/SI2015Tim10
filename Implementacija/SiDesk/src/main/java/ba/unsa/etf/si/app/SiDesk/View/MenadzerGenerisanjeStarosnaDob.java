d0b8bc98f3e7e7bc9ad77ac2448f030cad47095e
mirelapeskovic <mirelapeskovic@gmail.com>
2016-05-13 17:01:19 +0200

    Generisanje izvještaja po starosnoj dobi. Ako budete testirali morate imati podatke u tabelama: klijent, telefonskipoziv, i operater jer povezani tako i pretraga je po svim tabelama

--- Implementacija/SiDesk/src/main/java/ba/unsa/etf/si/app/SiDesk/View/MenadzerGenerisanjeStarosnaDob.java
+++ Implementacija/SiDesk/src/main/java/ba/unsa/etf/si/app/SiDesk/View/MenadzerGenerisanjeStarosnaDob.java
@@ -2,18 +2,25 @@ package ba.unsa.etf.si.app.SiDesk.View;
 
 import java.awt.Choice;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.MalformedURLException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+
+import ba.unsa.etf.si.app.SiDesk.ViewModel.GenerisiIzvjestajStarosnaDob;
 
 public class MenadzerGenerisanjeStarosnaDob {
 
 	protected JFrame frmGenerisanjeIzvjetaja;
 	private JTextField textField;
 	private JTextField textField_1;
-
+	private Choice choice;
 
 	/**
 	 * Launch the application.
@@ -48,36 +55,60 @@ public class MenadzerGenerisanjeStarosnaDob {
 		frmGenerisanjeIzvjetaja.setBounds(100, 100, 405, 239);
 		frmGenerisanjeIzvjetaja.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmGenerisanjeIzvjetaja.getContentPane().setLayout(null);
-		
+
 		JLabel lblOperater = new JLabel("Operater");
 		lblOperater.setBounds(43, 52, 56, 16);
 		frmGenerisanjeIzvjetaja.getContentPane().add(lblOperater);
-		
-		Choice choice = new Choice();
+
+		choice = new Choice();
 		choice.setBounds(177, 52, 175, 22);
+		choice.add("Foča");
+		choice.add("Ustikolina");
+		choice.add("Pretrovac");
+		choice.add("Gacko");
 		frmGenerisanjeIzvjetaja.getContentPane().add(choice);
-		
+
 		JLabel lblOd = new JLabel("Od:");
 		lblOd.setBounds(87, 107, 27, 16);
 		frmGenerisanjeIzvjetaja.getContentPane().add(lblOd);
-		
+
 		JLabel lblDo = new JLabel("Do:");
 		lblDo.setBounds(238, 107, 18, 16);
 		frmGenerisanjeIzvjetaja.getContentPane().add(lblDo);
-		
+
 		JButton btnKreirajIzvjetaj = new JButton("Kreiraj izvje\u0161taj");
 		btnKreirajIzvjetaj.setBounds(222, 153, 130, 25);
 		frmGenerisanjeIzvjetaja.getContentPane().add(btnKreirajIzvjetaj);
-		
+
 		textField = new JTextField();
 		textField.setBounds(124, 105, 86, 20);
 		frmGenerisanjeIzvjetaja.getContentPane().add(textField);
 		textField.setColumns(10);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
 		textField_1.setBounds(266, 105, 86, 20);
 		frmGenerisanjeIzvjetaja.getContentPane().add(textField_1);
-	}
 
+		btnKreirajIzvjetaj.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String command = e.getActionCommand();
+				GenerisiIzvjestajStarosnaDob i = new GenerisiIzvjestajStarosnaDob();
+				if (command.equals("Kreiraj izvje\u0161taj")) {
+					try {
+						GenerisiIzvjestajStarosnaDob.generisi(textField.getText(), textField_1.getText(),
+								choice.getSelectedItem());
+					} catch (MalformedURLException e1) {
+						JOptionPane.showMessageDialog(null, "Izvještaj nije generisan");
+					} catch (IOException e1) {
+						JOptionPane.showMessageDialog(null, "Izvještaj nije generisan");
+					}
+
+					JOptionPane.showMessageDialog(null, "Izvještaj je uspješno generisan");
+
+				} else
+					textField.setText("Nice");
+			}
+		});
+	}
 }
---
+++ Implementacija/SiDesk/src/main/java/ba/unsa/etf/si/app/SiDesk/ViewModel/GenerisiIzvjestajStarosnaDob.java
@@ -0,0 +1,144 @@
+package ba.unsa.etf.si.app.SiDesk.ViewModel;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFileChooser;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.Transaction;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+
+import ba.unsa.etf.si.app.SiDesk.Model.Klijent;
+import ba.unsa.etf.si.app.SiDesk.Model.Operater;
+import ba.unsa.etf.si.app.SiDesk.Model.TelefonskiPoziv;
+import ba.unsa.etf.si.app.SiDesk.Util.HibernateUtil;
+
+public final class GenerisiIzvjestajStarosnaDob {
+
+	public static Boolean generisi(String from, String to, String operater) throws MalformedURLException, IOException {
+
+		JFileChooser chooser = new JFileChooser();
+		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		int option = chooser.showSaveDialog(null);
+		if (option == JFileChooser.APPROVE_OPTION) {
+
+			String new_file_path = chooser.getSelectedFile().getAbsolutePath().toString() + "\\Izvjestaj.pdf";
+
+			try {
+
+				Session session = (Session) HibernateUtil.getSessionFactory().openSession();
+				Transaction t = session.beginTransaction();
+
+				Integer moreThan = Integer.parseInt(from);
+				Integer lessThan = Integer.parseInt(to);
+
+				Criteria criteria_klijent = session.createCriteria(Klijent.class);
+				criteria_klijent.add(Restrictions.gt("starost", moreThan));
+				criteria_klijent.add(Restrictions.lt("starost", lessThan));
+				List<Klijent> lista_klijenti = criteria_klijent.list();
+
+				Criteria criteria_poziv = session.createCriteria(TelefonskiPoziv.class);
+				List<TelefonskiPoziv> lista_pozivi = criteria_poziv.list();
+
+				Criteria criteria_operater = session.createCriteria(Operater.class);
+				criteria_operater.add(Restrictions.like("ime", operater));
+				List<Operater> lista_operateri = criteria_operater.list();
+
+				List<Klijent> klijenti = vratiKlijente(lista_klijenti, lista_operateri, lista_pozivi);
+
+				Document document = new Document();
+
+				PdfWriter pdfWriter = PdfWriter.getInstance(document, new FileOutputStream(new_file_path));
+
+				document.open();
+
+				Paragraph title = new Paragraph("Klijenti starosne dobi " + from + "-" + to + " operatera sa lokacije "
+						+ operater + " :" + " \n \n ", FontFactory.getFont(FontFactory.HELVETICA, 18, Font.BOLDITALIC));
+				document.add(title);
+
+				for (Klijent klijent : klijenti) {
+
+					PdfPTable pdfPTable = new PdfPTable(5);
+					PdfPCell pdfPCell1 = new PdfPCell(new Paragraph("Ime: " + klijent.getIme()));
+					PdfPCell pdfPCell2 = new PdfPCell(new Paragraph("Prezime: " + klijent.getPrezime()));
+					PdfPCell pdfPCell3 = new PdfPCell(new Paragraph("Broj telefona: " + klijent.getBrojTelefona()));
+					PdfPCell pdfPCell4 = new PdfPCell(new Paragraph("Godine : " + klijent.getStarost()));
+					PdfPCell pdfPCell5 = new PdfPCell(new Paragraph("Zaposlenje: " + klijent.getZaposlenje()));
+
+					pdfPTable.addCell(pdfPCell1);
+					pdfPTable.addCell(pdfPCell2);
+					pdfPTable.addCell(pdfPCell3);
+					pdfPTable.addCell(pdfPCell4);
+					pdfPTable.addCell(pdfPCell5);
+
+					document.add(pdfPTable);
+
+				}
+
+				document.close();
+				pdfWriter.close();
+
+			}
+
+			catch (FileNotFoundException e)
+
+			{
+
+				e.printStackTrace();
+
+			}
+
+			catch (DocumentException e)
+
+			{
+
+				e.printStackTrace();
+
+			}
+
+			if (Desktop.isDesktopSupported()) {
+				try {
+					File myFile = new File(new_file_path);
+					Desktop.getDesktop().open(myFile);
+				} catch (IOException ex) {
+					// no application registered for PDFs
+				}
+			}
+		}
+		return true;
+	}
+
+	private static ArrayList<Klijent> vratiKlijente(List<Klijent> klijenti, List<Operater> operateri,
+			List<TelefonskiPoziv> pozivi) {
+		ArrayList<Klijent> result = new ArrayList<Klijent>();
+		for (TelefonskiPoziv poziv : pozivi) {
+			for (Klijent klijent : klijenti) {
+				if (klijent.getId() == poziv.getKlijent().getId()
+						&& poziv.getOperater().getId() == operateri.get(0).getId()) {
+					result.add(klijent);
+				}
+			}
+		}
+
+		return result;
+
+	}
+
+}
