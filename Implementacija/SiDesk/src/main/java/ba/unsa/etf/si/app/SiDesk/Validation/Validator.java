42cf5247b0e3931b5feb450fd1c930ae535b27c6
avelija1 <almedin.velija@gmail.com>
2016-05-20 21:15:14 +0200

    Rijesen bug #36

--- Implementacija/SiDesk/src/main/java/ba/unsa/etf/si/app/SiDesk/Validation/Validator.java
+++ Implementacija/SiDesk/src/main/java/ba/unsa/etf/si/app/SiDesk/Validation/Validator.java
@@ -14,6 +14,9 @@ public class Validator {
 	private static final String Telefon_PATTERN1 = "([06]{2}[0-6]{1}[-]{1}[0-9]{3}[-]{1}[0-9]{3,})";
 	private static final String Telefon_PATTERN2 = "([06]{2}[0-6]{1}[/]{1}[0-9]{3}[-]{1}[0-9]{3,})";
 	private static final String ALPHANUMERIC_PATTERN = "^[a-zA-Z0-9 ]*$";
+	private static final String EMAIL_PATTERN = 
+			"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 //	private static final String NUMERIC_PATTERN = "^[0-9]*$";
 	
 	public static Boolean validairajJMBG(String jmbg) {
@@ -49,7 +52,9 @@ public class Validator {
 	}
 	
 	public static Boolean validirajEmail(String email) {
-		if(email.contains("@"))
+Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+		
+		if(pattern.matcher(email).matches() )
 			return true;
 		
 		else return false;
